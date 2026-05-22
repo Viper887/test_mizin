a8f0c65745537dda0ea972ad717b789e13c32085
svetlovskaya <svetlovskaya007@gmail.com>
2026-05-22 11:03:07 +0300

    Add python file
    ххіххіхіхіхіхіххііхі

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 import os
 print ("sosiska")
+class Тацій:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hello(self):
+        print(f"Привіт, мене звати {self.name}!")
+
+
+student = Тацій("Мілфа")
+student.say_hello()
